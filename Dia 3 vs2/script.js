2c4434dc08858c8cc05202021021d69d12e49c8a
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-05-14 15:56:55 -0300

    a3

---
+++ Dia 3 vs2/script.js
@@ -0,0 +1,12 @@
+var container = document.getElementById('.container')
+var fechar = document.getElementById('')
+var abrir = document.getElementById('abrir')
+
+function fechar () {
+    addEventListener('click, () => container.classList.add('mostrar')
+}
+
+
+
+
+
