cd5573c05aa7fbe049b3e9b512d2739ed99d526d
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-06-07 15:19:39 -0300

    Quadro de sons

---
+++ Dia 9/script.js
@@ -0,0 +1,26 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+// Para parar os sons quando outro botão for clicado
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
