22547dc33227c2ada9113b14233c668ca1ae2147
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-06-23 15:57:19 -0300

    Sorteiador (incompleto)

--- Dia A13 vs2/script.js
+++ Dia A13 vs2/script.js
@@ -1,6 +1,7 @@
 const caixatexto = document.getElementById('caixatexto');
 const tags = document.getElementById('tags');
 let i = 0;
+let aleatorio = 0;
 
 caixatexto.addEventListener('keyup', (event) => {
     let text = caixatexto.value.trim() 
@@ -10,13 +11,22 @@ caixatexto.addEventListener('keyup', (event) => {
         newElement()
     }
 
-    function newElement() {
-        const span = document.createElement('span');
-        span.classList.add('tag');
-        span.innerText += array[i];
-        tags.appendChild(span);
-        i++
+    if(event.key === 'Enter') {
+        random()
     }
 
-    console.log(array)
+    function newElement() {
+        const span = document.createElement('span');
+        span.classList.add(`tag`);
+        span.innerText += array[i];
+        tags.appendChild(span);
+        i++        
+    }
+
+    function random() {
+        let aleatorio = Math.floor(Math.random()*array.length);
+        
+    }
 })
+
+
