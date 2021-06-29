88db277838b1341d4c70f0c8435572917c92ea9f
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-06-29 10:51:06 -0300

    contador socialmedia

--- Dia A15 vs2/script.js
+++ Dia A15 vs2/script.js
@@ -1,5 +1,18 @@
 const numbers = document.querySelectorAll('.num');
 
 function counter() {
-    
+    numbers.forEach(number => {
+        const data = +number.getAttribute('data-target');
+        
+        const passo = Math.ceil(data / 200)        
+                
+        let res = passo;
+        
+               
+    })
 }
+
+counter()
+
+
+
