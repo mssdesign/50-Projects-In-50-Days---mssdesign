8ebaa8d4b9cf55a795278b8d8d6753cd070ff848
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-12-18 20:11:52 -0300

    50projects

---
+++ Loading_Content_ionic_cover/script.js
@@ -0,0 +1,23 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bgs_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+    header.innerHTML = '<img src="https://media.gettyimages.com/photos/laptop-computer-blank-white-screen-on-table-in-cafe-background-laptop-picture-id1276979385?s=2048x2048" alt=""/>'
+    title.innerHTML = 'Lorem ipsum dolor sit amet.'
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, assumenda.'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+    name.innerHTML = 'Marcos Paulo'
+    date.innerHTML = '08/11/2021'
+
+    animated_bgs.forEach(bg => bg.classList).remove('animated-bg')
+    animated_bgs_texts.forEach(bg => bg.classList).remove('animated-bg-text')
+}
+
